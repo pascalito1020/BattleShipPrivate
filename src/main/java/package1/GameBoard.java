4485e8e59df2a1f7ef606c80f75f4788df2538fa
Pascal <pascal_lotter@yahoo.com>
2022-01-12 21:00:42 +0100

    test commit

--- src/main/java/package1/GameBoard.java
+++ src/main/java/package1/GameBoard.java
@@ -1,17 +1,15 @@
 package package1;
 
-import java.sql.SQLOutput;
-import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
-
 
 public class GameBoard {
 
     public char[][] enemyBoard = new char[10][10];
 
     public char[][] playerBoard = new char[10][10];
-    char water = 'o';
-    char ship = 'X';
+    char water = 'W';
+    char ship = 'S';
     public int shipNumber = 5;
 
     public char[][] createGameBoard() {
@@ -27,9 +25,7 @@ public class GameBoard {
                 playerBoard[row][col] = water;
             }
 
-
         }
-        System.out.println("");
 
         return playerBoard;
     }
@@ -64,7 +60,7 @@ public class GameBoard {
 
             switch (choose) {
                 case 'u':
-                    if (y1+1 - shipLength < 0) {
+                    if (y1 + 1 - shipLength < 0) {
                         System.out.println("Cant be placed like that!");
                         playerBoard[y1][x1] = water;
                         break;
@@ -80,7 +76,7 @@ public class GameBoard {
 
                 case 'd':
 
-                    if (y1-1 + shipLength > 9) {
+                    if (y1 - 1 + shipLength > 9) {
                         System.out.println("Cant be placed like that!");
                         playerBoard[y1][x1] = water;
                         break;
@@ -96,13 +92,13 @@ public class GameBoard {
 
                 case 'l':
 
-                    if (x1+1 - shipLength < 0) {
+                    if (x1 + 1 - shipLength < 0) {
                         System.out.println("Cant be placed like that!");
                         playerBoard[y1][x1] = water;
                         break;
                     }
                     for (int i = 0; i < shipLength; i++) {
-                        playerBoard[y1][x1-i] = playerBoard[y1][x1];
+                        playerBoard[y1][x1 - i] = playerBoard[y1][x1];
                     }
 
                     showGameBoard();
@@ -112,14 +108,14 @@ public class GameBoard {
 
                 case 'r':
 
-                    if (x1-1 + shipLength > 9) {
+                    if (x1 - 1 + shipLength > 9) {
                         System.out.println("Cant be placed like that!");
                         playerBoard[y1][x1] = water;
                         break;
                     }
 
                     for (int i = 0; i < shipLength; i++) {
-                        playerBoard[y1][x1+i] = playerBoard[y1][x1];
+                        playerBoard[y1][x1 + i] = playerBoard[y1][x1];
                     }
 
                     showGameBoard();
@@ -133,10 +129,7 @@ public class GameBoard {
 
         }
 
-        System.out.println("All ships placed!");
-
     }
-
 
     public void showGameBoard() {
 
@@ -156,6 +149,98 @@ public class GameBoard {
 
     }
 
+    public void createEnemyBoard() {
+
+        int random1;
+        int random2;
+
+        Random zufall = new Random();
+
+        random1 = zufall.nextInt(9); /** Erstellung des 2er Schiffs**/
+        random2 = zufall.nextInt(9);
+
+
+        if (random1 + 2 > 9) {
+
+            random1 = random1 - 2;
+        }
+
+        for (int i = 0; i < 2; i++) {
+
+            enemyBoard[random1 + i][random2] = ship;
+
+        }
+
+
+        random1 = zufall.nextInt(9); /** Erstellung des 5er Schiff **/
+        random2 = zufall.nextInt(9);
+
+        if (random1 + 5 > 9) {
+
+            random1 = random1 - 5;
+
+        }
+
+        for (int i4 = 0; i4 < 5; i4++) {
+
+            enemyBoard[random1 + i4][random2] = ship;
+
+        }
+
+
+        for (int u = 0; u < 2; u++) { /** Schleife für die Erstellung von 2 3er Schiffe **/
+
+
+            random1 = zufall.nextInt(9);
+            random2 = zufall.nextInt(9);
+
+            if (random2 - 3 < 0) {
+
+                random2 = random2 + 3;
+            }
+
+
+            for (int i2 = 0; i2 < 3; i2++) {
+
+
+                enemyBoard[random1][random2 - i2] = ship;
+
+            }
+
+        }
+
+        random1 = zufall.nextInt(9); /** Erstellung von 4er Schiff **/
+        random2 = zufall.nextInt(9);
+
+        if (random2 - 4 < 0) {
+
+            random2 = random2 + 4;
+        }
+        for (int i3 = 0; i3 < 4; i3++) {
+
+            enemyBoard[random1][random2 - i3] = ship;
+
+        }
+
+    }
+
+    public void showEnemyBoard() {
+
+        for (int row = 0; row < enemyBoard.length; row++) {
+
+            System.out.println(" ");
+
+            for (int col = 0; col < enemyBoard.length; col++) {
+
+                System.out.print(enemyBoard[row][col] + " ");
+
+            }
+
+        }
+        System.out.println("");
+
+
+    }
 
 
 }
---
+++ src/main/java/package1/Main.java
@@ -0,0 +1,37 @@
+package package1;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        GameBoard game = new GameBoard();
+/**
+
+     game.createGameBoard();
+
+     System.out.println("");
+
+        game.placeShips(game.playerBoard);
+
+
+
+ for (int row = 0; row < game.playerBoard.length; row++) {
+
+ System.out.println(" ");
+
+ for (int col = 0; col < game.playerBoard.length; col++) {
+
+ System.out.print(game.playerBoard[row][col] + " ");
+
+ }
+
+ }
+ **/
+
+        game.createEnemyBoard();
+        game.showEnemyBoard();
+
+
+    }
+}
